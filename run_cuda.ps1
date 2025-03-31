a93d0446c512a789397daf23d2e84efdaa745377
its5Q <leshaegoroov@gmail.com>
2025-04-01 00:39:28 +1000

    Fix run_cuda.ps1

--- run_cuda.ps1
+++ run_cuda.ps1
@@ -1,4 +1,5 @@
 .\conda_install\Scripts\conda shell.powershell hook | Out-String | ?{$_} | Invoke-Expression
 conda activate .\conda_env
-set IZABELA_USE_CUDA=1
+$env:IZABELA_USE_CUDA=1
+$env:Path = "$env:Path;$pwd\conda_env\Library\bin"
 fastapi run
